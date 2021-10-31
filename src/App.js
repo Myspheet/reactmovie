fb93984420e8eccf39d04350dae7b69470e67786
Promise Oghenevwefe <promise.oghenevwefe@scandiweb.com>
2021-10-31 19:42:11 +0100

    Creates and styles header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Components
+import Header from './components/Header';
+
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -7,6 +10,7 @@ import { GlobalStyle } from './GlobalStyle';
 const App = () => {
   return (
     <div className="App">
+      <Header />
       Start here.
       <GlobalStyle />
     </div>
