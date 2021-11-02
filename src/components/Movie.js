dff73d60da9ca66fad1201814eb195acf075cd9c
Promise Oghenevwefe <promise.oghenevwefe@scandiweb.com>
2021-11-02 14:53:44 +0100

    Creates and Styles movie info bar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -17,15 +18,16 @@ import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
     const { movieId } = useParams();
-    const { state: movie, loading, error} = useMovieFetch(movieId);
+    const { state: movie, loading, error } = useMovieFetch(movieId);
 
-    if(loading) return <Spinner />
-    if(error) return <div>Something went wrong...</div>
+    if (loading) return <Spinner />
+    if (error) return <div>Something went wrong...</div>
 
     return (
         <>
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
+            <MovieInfoBar time={movie.runtime} budget={movie.budge} revenue={movie.revenue} />
         </>
     );
 }
